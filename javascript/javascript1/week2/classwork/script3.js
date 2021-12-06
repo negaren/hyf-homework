3147a304fb6c5e75f131a5dbbbec9c0fd302845e
Negar Eghbali <negar.en@gmail.com>
2021-12-07 00:54:56 +0100

    classwork

---
+++ javascript/javascript1/week2/classwork/script3.js
@@ -0,0 +1,29 @@
+
+const friendList = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'ajj', 'Kay', 'Bruce'];
+let count = 0;
+for (let i = 0; i <= friendList.length; i++){
+    if( friendList[i].includes('a')===true){
+        console.log(count+=1);
+    } else {
+        console.log(friendList[i]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
