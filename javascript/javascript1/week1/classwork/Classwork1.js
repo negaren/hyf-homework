47bf1ab8bc1f3eaa2ea62fc4bc9d3af425d6d92e
Negar Eghbali <negar.en@gmail.com>
2021-12-07 00:05:46 +0100

    adding Classwork

---
+++ javascript/javascript1/week1/classwork/Classwork1.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+
+
+
+
+let myName = 'Default';
+myName = 'John'
+
+let myAge = 35;
+
+// add your code here
+
+
+// DO NOT change the code below
+console.log(`In 20 years, ${myName} will be ${myAge + 20}`)
