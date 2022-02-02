7a47aea2a6c98ec8c0efccd2184f0f1f1d55bf4a
Negar Eghbali <negar.en@gmail.com>
2022-02-02 13:52:49 +0100

    database week1 homework

--- javascript/javascript2/week1/classwork/Simple eventlistener/index.js
+++
@@ -1,9 +0,0 @@
-console.log("Negar")
-
-const myButton = document.querySelector('button')
-myButton.addEventListener('click', function(){
-    if (myButton.innerText == 'Click Here'){
-        myButton.innerText = 'Button Clicked'
-    } else
-    myButton.innerText = 'Click Here'
-})
