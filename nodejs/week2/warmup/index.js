a6bbd605e45116279b56d2ad117184de8400e930
Negar Eghbali <negar.en@gmail.com>
2022-02-28 21:58:04 +0100

    nodejs week2 materials

--- nodejs/week2/warmup/index.js
+++ nodejs/week2/warmup/index.js
@@ -3,4 +3,14 @@ const app = express();
 
 app.get("/", (req, res) => res.send("nodejs week2 homework"));
 
+app.get("/numbers/add", (req, res) => {
+    const firstNum = Number(req.query.first);
+    const secondNum = Number(req.query.second);
+    res.send(`${firstNum + secondNum}`);
+})
+
+app.get("/numbers/multiply", (req, res) => {
+    res.send(`${Number(req.query.first) * Number(req.query.second)}`);
+})
+
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
